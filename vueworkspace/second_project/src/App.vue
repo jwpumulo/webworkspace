e87f62cce54888c610d14ced243e6737500bb7f5
Jw Pumulo <j.w.pumulo@gmail.com>
2025-04-01 17:48:18 +0900

    20250401

---
+++ vueworkspace/second_project/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <input type="text" v-model.lazy="selected">
+    <component v-bind:is="selected"></component> <!-- 임의 컴포넌트 is-->
+  </div>
+</template>
+
+<script>
+  import PropsCom from './components/PropsComponent.vue';
+  import TextModal from './views/TextModal.vue';
+
+  
+  export default {
+    data() {
+      return {
+        selected: 'PropsCom'
+      }
+    },
+    components: {
+      PropsCom, // 'PropsCom': PropsCom 이 이름을 찾
+      TextModal,
+    },
+    created() {
+      console.log('App.vue Created');
+    },
+    mounted() {
+      console.log('App.vue Mounted');
+    },
+  }
+</script>
+
+
+
+
+
+
+
+
+
+
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  nav {
+    padding: 30px;
+  }
+
+  nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  nav a.router-link-exact-active {
+    color: #42b983;
+  }
+</style>
---
+++ vueworkspace/second_project/src/components/PageTitle.vue
@@ -0,0 +1,21 @@
+<!-- components/PageTitle.vue 자식-->
+<template>
+  <h2> {{ title }}</h2>
+</template>
+
+<script>
+  export default {
+
+    
+
+    //부모가 넘겨준 값들 read only
+    props: ['title'],
+    created() {
+      console.log('PropsComponent.vue Created');
+    },
+    mounted() {
+      console.log('PropsComponent.vue Mounted');
+    }
+
+  }
+</script>
